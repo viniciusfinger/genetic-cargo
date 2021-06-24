329d88bade7faf32c77a1276ef3ccc2882e3a0f7
Vinicius Finger <vinicius.finger2018@gmail.com>
2021-06-24 20:33:07 -0300

    Alterações na geração de tabelas e refatoração de alguns métodos

--- auxiliar.py
+++ auxiliar.py
@@ -16,6 +16,17 @@ def instanciaItens() -> list:
 
     return itens
 
+def geraTabelaConfiguracoes(capacidadeDeCarga, numeroDeIndividuos, geracoes):
+    configuracoesJson = {"Capacidade de Carga":str(capacidadeDeCarga)+" kg","Número de Indivíduos":numeroDeIndividuos,"Número de Gerações":geracoes}
+
+    html_file = open("tabelaConfiguracoes.html","w")
+    tabelaConfiguracoes = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>'
+    tabelaConfiguracoes += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">'
+    tabelaConfiguracoes += json2html.convert(json=configuracoesJson,table_attributes='style="width: 100%;"')
+  
+    html_file.write(tabelaConfiguracoes)
+    html_file.close()
+
 def processaMelhorIndividuo(populacao, itens):
     cincoUltimosIndividuos = []
     indiceMelhorIndividuo = 0
